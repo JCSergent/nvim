a3a861a85d0b032803beec3df45c0a98102b1e38
JC Sergent <jcsergent20@gmail.com>
2025-03-23 16:41:58 -0400

    'toggterm'

--- lua/jc/plugins/toggleterm.lua
+++ lua/jc/plugins/toggleterm.lua
@@ -7,8 +7,8 @@ return {
         vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm direction=horizontal<CR>', {})
         vim.keymap.set('n', '<leader>tn', '<cmd>TermNew direction=horizontal<CR>', {})
 
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n><leader>tt]], {})
-        --        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], {})
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], {})
+        vim.keymap.set('t', '<C-v>', [[<C-\><C-n>]], {})
         --      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], {})
         --    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], {})
         --  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], {})
