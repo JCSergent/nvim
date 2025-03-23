f126ff8a7233d58a28c20b89e7c4c682caa38527
JC Sergent <jcsergent20@gmail.com>
2025-03-23 16:30:31 -0400

    'termtoggle'

---
+++ lua/jc/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+    'akinsho/toggleterm.nvim',
+    config = function ()
+        require('toggleterm').setup()
+
+--        vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm direction=float<CR>', {})
+        vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm direction=horizontal<CR>', {})
+        vim.keymap.set('n', '<leader>tn', '<cmd>TermNew direction=horizontal<CR>', {})
+
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n><leader>tt]], {})
+        --        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], {})
+        --      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], {})
+        --    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], {})
+        --  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], {})
+        --vim.keymap.set('t', '<C-w>', [[<C-\><C-w><C-w>]], {})
+    end
+}
--- lua/jc/remap.lua
+++ lua/jc/remap.lua
@@ -10,3 +10,12 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+
+-- Window Navigation
+vim.keymap.set('n', '<tab>', '<C-W><C-W>')
+vim.keymap.set('n', '<C-k>', '<C-W>k')
+vim.keymap.set('n', '<C-j>', '<C-W>j')
+vim.keymap.set('n', '<C-h>', '<C-W>h')
+vim.keymap.set('n', '<C-l>', '<C-W>l')
+vim.keymap.set('n', '<leader><tab>', '<C-W><C-Q>')
