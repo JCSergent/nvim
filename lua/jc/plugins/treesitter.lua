4b471977cac434da1f5cf714db2a9b771b05aa31
JC Sergent <jcsergent20@gmail.com>
2025-03-31 14:31:41 -0400

    living large with a lsp

--- lua/jc/plugins/treesitter.lua
+++ lua/jc/plugins/treesitter.lua
@@ -1,45 +1,46 @@
 return {
     {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc",
-                "javascript",
-                "typescript",
-                "lua",
-                "jsdoc",
-                "bash",
-                "cpp"
-            },
-            auto_install = true,
-            indent = {
-                enable = true
-            },
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    if lang == "html" then
-                        print("disabled")
-                        return true
-                    end
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all"
+                ensure_installed = {
+                    "vim",
+                    "vimdoc",
+                    "javascript",
+                    "typescript",
+                    "lua",
+                    "jsdoc",
+                    "bash",
+                    "cpp"
+                },
+                auto_install = true,
+                indent = {
+                    enable = true
+                },
+                highlight = {
+                    enable = true,
+                    disable = function(lang, buf)
+                        if lang == "html" then
+                            print("disabled")
+                            return true
+                        end
 
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        vim.notify(
-                            "File larger than 100KB treesitter disabled for performance",
-                            vim.log.levels.WARN,
-                            {title = "Treesitter"}
-                        )
-                        return true
-                    end
-                end,
-                additional_vim_regex_highlighting = false,
-            },
-        })
-    end
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            vim.notify(
+                                "File larger than 100KB treesitter disabled for performance",
+                                vim.log.levels.WARN,
+                                {title = "Treesitter"}
+                            )
+                            return true
+                        end
+                    end,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end
     },
 }
